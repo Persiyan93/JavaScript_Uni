bb19d135c852c8b01086d78c51d51ed74f50a41e
Persiyan93 <persiyan.georgiev@gmail.com>
2020-10-31 23:12:28 +0200

    Phonebook

--- Rest services and AJAX/04. JS-Applications-REST-Services-and-AJAX-Exercise-Resources/03.PHONEBOOK/app.js
+++ Rest services and AJAX/04. JS-Applications-REST-Services-and-AJAX-Exercise-Resources/03.PHONEBOOK/app.js
@@ -1,5 +1,39 @@
 function attachEvents() {
-    console.log('TODO...');
+    let httpRequest = new XMLHttpRequest();
+    let ulPhoneBook = document.querySelector('#phonebook');
+    let createButton = document.querySelector('#btnCreate');
+    createButton.addEventListener('click', addContact)
+
+    let loadButton = document.querySelector('#btnLoad');
+    loadButton.addEventListener('click', load);
+    function deletePerson(e) {
+        let personket=e.currentTarget.parentElement.getAttribute('key');
+        console.log(personket);
+        let url = ` https://phonebook-nakov.firebaseio.com/phonebook/.json`
+    }
+    function addContact(e) {
+
+    }
+    function load() {
+        let url = `https://phonebook-nakov.firebaseio.com/phonebook.json`;
+        httpRequest.open('GET', url);
+        httpRequest.send();
+        httpRequest.addEventListener('loadend', () => {
+            let phonebook = JSON.parse(httpRequest.responseText);
+            for (const key in phonebook) {
+                let liElement = document.createElement('li');
+                console.log(phonebook[key]);
+                liElement.innerHTML = `${phonebook[key].person}: ${phonebook[key].phone}`
+                liElement.setAttribute('key', key);
+                let deleteButton = document.createElement('button');
+                deleteButton.innerHTML = 'Delete';
+                deleteButton.addEventListener('click', deletePerson);
+                liElement.appendChild(deleteButton);
+                ulPhoneBook.appendChild(liElement);
+            }
+
+        })
+    }
 }
 
 attachEvents();
