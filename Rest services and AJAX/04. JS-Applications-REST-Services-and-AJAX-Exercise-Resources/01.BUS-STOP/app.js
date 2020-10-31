634e5139fb996558870ca7899722acd503892511
Persiyan93 <persiyan.georgiev@gmail.com>
2020-10-31 02:41:21 +0200

    Bus Stop

--- Rest services and AJAX/04. JS-Applications-REST-Services-and-AJAX-Exercise-Resources/01.BUS-STOP/app.js
+++ Rest services and AJAX/04. JS-Applications-REST-Services-and-AJAX-Exercise-Resources/01.BUS-STOP/app.js
@@ -2,10 +2,25 @@ function getInfo() {
     let stopId = document.querySelector("#stopId")
     let url = `https://judgetests.firebaseio.com/businfo/${stopId.value}.json`;
     let httpRequest = new XMLHttpRequest();
-    httpRequest.addEventListener('onload',(e)=>{
-        console.log(e.currentTarget);
+    httpRequest.addEventListener('loadend', (e) => {
+        let stopName = document.querySelector('#stopName');
+        if (httpRequest.status != 200) {
+            stopName.innerHTML = 'Error';
+        }
+        else {
+            let info = JSON.parse(httpRequest.responseText);
+            stopName.innerHTML = info.name;
+            let ulElement = document.querySelector('#buses');
+            for (const key in info.buses) {
+                let lielement = document.createElement('li');
+                lielement.innerHTML = `Bus ${key} arrives in ${info.buses[key]} minutes`;
+                ulElement.appendChild(lielement);
+            }
+        }
+
     });
-    httpRequest.open('GET',url);
+    httpRequest.open('GET', url);
     httpRequest.send();
-    console.log(httpRequest);
+   
+    
 }
