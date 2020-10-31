cece5194f6be1d1b6581c7455d9020bdf2bbb4ca
Persiyan93 <persiyan.georgiev@gmail.com>
2020-10-31 22:16:44 +0200

    Bus Shedule

--- Rest services and AJAX/04. JS-Applications-REST-Services-and-AJAX-Exercise-Resources/02.BUS-SCHEDULE/app.js
+++ Rest services and AJAX/04. JS-Applications-REST-Services-and-AJAX-Exercise-Resources/02.BUS-SCHEDULE/app.js
@@ -1,11 +1,38 @@
 function solve() {
+    let nextStopId = 'depot';
+    let stopName;
+    let departButton = document.querySelector('#depart');
+    let arriveButton = document.querySelector('#arrive');
+    let currentstop = document.querySelector('#info>span');
 
-    function depart() {
-        console.log('Depart TODO...');
+
+    function depart(e) {
+        let httpRequest = new XMLHttpRequest();
+        httpRequest.addEventListener('loadend', () => {
+            if (httpRequest.status != 200) {
+                currentstop.innerHTML = `Error`;
+                departButton.disabled = true;
+                arriveButton.disabled = true;
+            } else {
+                let stop = JSON.parse(httpRequest.responseText);
+                stopName = stop.name;
+                nextStopId = stop.next;
+                currentstop.innerHTML = `Next stop ${stop.name}`;
+            }
+        });
+        let url = `https://judgetests.firebaseio.com/schedule/${nextStopId}.json `;
+        httpRequest.open('GET', url);
+        httpRequest.send();
+        arriveButton.disabled = false;
+        departButton.disabled = true;
+
+
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        currentstop.innerHTML = `Arriving at ${stopName}`;
+        departButton.disabled = false;
+        arriveButton.disabled = true;
     }
 
     return {
