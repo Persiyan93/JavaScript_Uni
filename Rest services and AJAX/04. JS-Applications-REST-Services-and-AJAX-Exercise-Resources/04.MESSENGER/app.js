19249ab342c653d1ba8d81c105bb03b119038800
Persiyan93 <persiyan.georgiev@gmail.com>
2020-11-01 02:06:49 +0200

    Messanger

--- Rest services and AJAX/04. JS-Applications-REST-Services-and-AJAX-Exercise-Resources/04.MESSENGER/app.js
+++ Rest services and AJAX/04. JS-Applications-REST-Services-and-AJAX-Exercise-Resources/04.MESSENGER/app.js
@@ -1,5 +1,43 @@
 function attachEvents() {
-    console.log('TODO...');
+    let url = `https://rest-messanger.firebaseio.com/messanger.json`;
+    let elements = {
+        sendButton: document.querySelector('#submit'),
+        refreshButton: document.querySelector('#refresh'),
+        name: document.querySelector('#author'),
+        message: document.querySelector('#content'),
+        messages:document.querySelector('#messages')
+
+    }
+    elements.sendButton.addEventListener('click', sendMessage);
+    elements.refreshButton.addEventListener('click', refresh)
+    function sendMessage() {
+        let personMessage = {
+            author: elements.name.value,
+            content: elements.message.value
+        }
+        let data = JSON.stringify(personMessage);
+        let httpRequest = new XMLHttpRequest();
+        httpRequest.open('POST', url);
+        httpRequest.send(data);
+        console.log(data);
+        httpRequest.addEventListener('loadend',()=>{
+            console.log(httpRequest);
+        })
+    }
+    function refresh() {
+        let httpRequest = new XMLHttpRequest;
+        
+        httpRequest.addEventListener('loadend',()=>{
+           let messages=(JSON.parse(httpRequest.responseText))
+            for (const key in messages) {
+                console.log(key);
+               elements.messages.value+=`${messages[key].author}: ${messages[key].content}+\n`;
+            }
+           
+        })
+        httpRequest.open('GET',url);
+        httpRequest.send();
+    }
 }
 
 attachEvents();
