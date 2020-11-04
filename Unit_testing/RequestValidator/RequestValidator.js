fdaa287b6e05894c606e173d73224c88de921570
Persiyan93 <persiyan.georgiev@gmail.com>
2020-11-04 22:26:11 +0200

    Unit testing

--- Unit_testing/RequestValidator/RequestValidator.js
+++ Unit_testing/RequestValidator/RequestValidator.js
@@ -1,5 +1,29 @@
-function validator(http) {
-    if (http[method] != 'GET' && http[method] != 'POST' && http[method] != 'DELETE' &&http[method]!='CONNECT') {
-        throw new Error('Invalid request header: Invalid {Method}');
+function validator(httpRequest) {
+    let properties = ['method', 'uri', 'version', 'message'];
+    let uriRegex = new RegExp('^[A-Za-z0-9\.\*]+$');
+    let validVersion = ['HTTP/0.9', 'HTTP/1.0', 'HTTP/1.1', 'HTTP/2.0']
+    let validMethods = ['GET', 'POST', 'DELETE', 'CONNECT'];
+    let validMessage = new RegExp(`^[^<>\\&',"]+$`);
+    let message = '';
+    function validate(httpRequest) {
+        properties.forEach(element => {
+            if (!httpRequest.hasOwnProperty(element)) {
+                message = element.charAt(0).toUpperCase() + element.slice(1);
+            }
+        });
+        if (!validMethods.includes(httpRequest.method)) {
+            message = 'Method';
+        }
+        else if (!httpRequest.uri.match(uriRegex)) {
+            message = 'URI';
+        }
+        else if (!validVersion.includes(httpRequest.version)) {
+            message = 'Version';
+        }
+        if (message != '') {
+            throw new Error(`Invalid request header: Invalid ${message}`);
+        }
+        return httpRequest;
+
     }
 }
