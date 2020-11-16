d7f52458502caed3557213104b6d1e327f080644
Persiyan93 <persiyan.georgiev@gmail.com>
2020-11-16 11:01:09 +0200

    Cats status

--- 07. JS-Applications-Templating-Exercise-Resources/02. HTTP Status Cats/template.js
+++ 07. JS-Applications-Templating-Exercise-Resources/02. HTTP Status Cats/template.js
@@ -1,8 +1,29 @@
-(() => {
-     renderCatTemplate();
 
-     function renderCatTemplate() {
-         // TODO: Render cat template and attach events
-     }
- 
+window.addEventListener('load', () => {
+    console.log('dasdda');
+    let catTemplate = document.querySelector('#cat-template').innerHTML;
+    let templateFunc = Handlebars.compile(catTemplate);
+    //Generate HTML
+    let generatedHtml = cats.map(cat => templateFunc(cat)).join('');
+    //Append generated HTML
+    let ulElement = document.querySelector('#allCats').children[0];
+    ulElement.innerHTML = generatedHtml;
+    //Add eventListener
+    ulElement.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (e.target.tagName==='BUTTON') {
+            if (e.target.parentElement.children[1].style.display === 'block') {
+                e.target.parentElement.children[1].style.display = 'none';
+            }
+            else {
+                e.target.parentElement.children[1].style.display = 'block';
+            }
+
+        }
+
+    })
+
+
+
 })
+
