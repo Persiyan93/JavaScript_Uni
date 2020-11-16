dbc325e86f55676162c0191d8e067c96685493c6
Persiyan93 <persiyan.georgiev@gmail.com>
2020-11-16 08:53:41 +0200

    List towns

--- 07. JS-Applications-Templating-Exercise-Resources/01. List Towns/app.js
+++ 07. JS-Applications-Templating-Exercise-Resources/01. List Towns/app.js
@@ -1,1 +1,19 @@
-console.log(Handlebars)
+window.addEventListener('load', () => {
+    let button = document.querySelector('#btnLoadTowns');
+    //create  template function
+    let templatestring = document.querySelector('#template').innerHTML;
+    let templateFunc = Handlebars.compile(templatestring);
+    //addd eventListener
+    button.addEventListener('click', (e) => {
+        e.preventDefault()
+        //read input
+        let inputEl = document.querySelector('#towns');
+        let towns = inputEl.value.split(', ');
+        //generate HTML
+        let generatedHtml = templateFunc({ towns });
+        //Append html to DOM tree
+        let ulElement = document.querySelector('#root').children[0];
+        ulElement.innerHTML += generatedHtml;
+
+    });
+})
