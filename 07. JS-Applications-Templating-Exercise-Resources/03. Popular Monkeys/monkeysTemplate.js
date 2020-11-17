6e7feb1876fda1ded28b2cc6dd3c2c6449db0614
Persiyan93 <persiyan.georgiev@gmail.com>
2020-11-17 11:18:27 +0200

    Popular Monkey

--- 07. JS-Applications-Templating-Exercise-Resources/03. Popular Monkeys/monkeysTemplate.js
+++ 07. JS-Applications-Templating-Exercise-Resources/03. Popular Monkeys/monkeysTemplate.js
@@ -1,3 +1,20 @@
-$(() => {
-    // TODO
+$(async () => {
+    let mainTemplate = await (await fetch('./mainTemplate.hbs')).text();
+    let monkeyTemplate = document.querySelector('#monkey-template').innerHTML;
+    Handlebars.registerPartial('monkey', monkeyTemplate);
+    let templateFunc = Handlebars.compile(mainTemplate);
+    let generatedHtml=templateFunc({monkeys});
+    let sectionElement=document.querySelector('section');
+    sectionElement.innerHTML=generatedHtml;
+    sectionElement.addEventListener('click',(e)=>{
+        if (e.target.tagName=='BUTTON') {
+           e.target.parentElement.querySelector('p').removeAttribute('style');
+        }
+    })
+    
+   
+   
+    
+   
+  
 })
