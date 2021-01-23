94cbec76e190a5581d4422673323b140f7cccaa2
Persiyan93 <persiyan.georgiev@gmail.com>
2021-01-23 14:49:55 +0200

    Add firebase cofig file

---
+++ Quize-game/src/cofigFirebase.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAtyHVzgxAFr3ZeWZ62238caKtg8f-geV8",
+    authDomain: "quiz-game-75f05.firebaseapp.com",
+    projectId: "quiz-game-75f05",
+    storageBucket: "quiz-game-75f05.appspot.com",
+    messagingSenderId: "294415474072",
+    appId: "1:294415474072:web:1e905653facd353bdf1fed"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
