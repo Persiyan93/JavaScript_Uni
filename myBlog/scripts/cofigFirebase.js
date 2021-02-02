2ce32f2ea86662939aeb9101a94b2928b0aa9238
Persiyan93 <persiyan.georgiev@gmail.com>
2021-02-02 20:17:58 +0200

    Add firebase Config file

--- myBlog/scripts/cofigFirebase.js
+++ myBlog/scripts/cofigFirebase.js
@@ -1,11 +1,11 @@
- // Your web app's Firebase configuration
- var firebaseConfig = {
-    apiKey: "AIzaSyCx1l9MCIapDQVqcUJuCJNUXNWQ_1vxeLI",
-    authDomain: "softwiki-976ab.firebaseapp.com",
-    projectId: "softwiki-976ab",
-    storageBucket: "softwiki-976ab.appspot.com",
-    messagingSenderId: "249851727132",
-    appId: "1:249851727132:web:f3c83b06d2ab1eed3c3ed9"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+var firebaseConfig = {
+  apiKey: "AIzaSyBLjUDn73YyAGHnFV4zvfV7obMkUp7k5Rk",
+  authDomain: "movies-50730.firebaseapp.com",
+  databaseURL: "https://movies-50730.firebaseio.com",
+  projectId: "movies-50730",
+  storageBucket: "movies-50730.appspot.com",
+  messagingSenderId: "254321052718",
+  appId: "1:254321052718:web:0297c51eb033f734b263bf"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
